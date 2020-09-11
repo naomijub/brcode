d19131ca80fbe99ed0e3f20c407d742ddb81b8fe
Julia Naomi <jnboeira@outlook.com>
2020-09-10 23:04:49 -0300

    include templates

--- src/model.rs
+++ src/model.rs
@@ -5,7 +5,7 @@ use crate::parse::Data;
 pub struct BrCode {
     payload_version: u8,
     initiation_methos: Option<u8>,
-    // merchant_information: Vec<Info>,
+    merchant_information: Vec<MerchantInfo>,
     merchant_category_code: u32,
     merchant_name: String,
     merchant_city: String,
@@ -13,15 +13,27 @@ pub struct BrCode {
     currency: String,
     amount: Option<f64>,
     country_code: String,
-    // field_template: Vec<Label>,
+    field_template: Vec<Label>,
     crc1610: String,
-    // templates: Option<Vec<(usize, Data)>>
+    templates: Option<Vec<Template>>,
 }
 
-// #[derive(Debug, PartialEq)]
-// pub struct Label {
-//     reference_label: String,
-// }
+#[derive(Debug, PartialEq)]
+pub struct Label {
+    reference_label: String,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct MerchantInfo {
+    id: usize,
+    info: Vec<Info>,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Template {
+    id: usize,
+    info: Vec<Info>,
+}
 
 #[derive(Debug, PartialEq)]
 pub struct Info {
@@ -32,10 +44,53 @@ pub struct Info {
 impl From<Vec<(usize, Data)>> for BrCode {
     fn from(code: Vec<(usize, Data)>) -> Self {
         let hash = HashBrCode::new(code).0;
+        let merchant_information = (26usize..=51usize)
+            .map(|i| (i, hash.get(&i).clone()))
+            .filter_map(|e| {
+                e.1.map(|d| {
+                    Some(MerchantInfo {
+                        id: e.0,
+                        info: {
+                            let hm = d.to_hash();
+                            let mut keys = hm.keys().map(|k| *k).collect::<Vec<usize>>();
+                            keys.sort();
+                            keys.into_iter()
+                                .map(|idx| Info {
+                                    id: idx,
+                                    info: hm[&idx].to_str(),
+                                })
+                                .collect::<Vec<Info>>()
+                        },
+                    })
+                })?
+            })
+            .collect::<Vec<MerchantInfo>>();
+        let templates = (80usize..=99usize)
+            .map(|i| (i, hash.get(&i).clone()))
+            .filter_map(|e| {
+                e.1.map(|d| {
+                    Some(Template {
+                        id: e.0,
+                        info: {
+                            let hm = d.to_hash();
+                            let mut keys = hm.keys().map(|k| *k).collect::<Vec<usize>>();
+                            keys.sort();
+                            keys.into_iter()
+                                .map(|idx| Info {
+                                    id: idx,
+                                    info: hm[&idx].to_str(),
+                                })
+                                .collect::<Vec<Info>>()
+                        },
+                    })
+                })?
+            })
+            .collect::<Vec<Template>>();
+
         BrCode {
             payload_version: hash[&0usize].to_str().parse().unwrap(),
             initiation_methos: hash.get(&1usize).map(|e| e.to_str().parse().unwrap()),
-            // merchant_information: ,
+            merchant_information: merchant_information,
             merchant_category_code: hash[&52usize].to_str().parse().unwrap(),
             merchant_name: hash[&59usize].to_str(),
             merchant_city: hash[&60usize].to_str(),
@@ -43,9 +98,15 @@ impl From<Vec<(usize, Data)>> for BrCode {
             currency: hash[&53usize].to_str(),
             amount: hash.get(&54usize).map(|e| e.to_str().parse().unwrap()),
             country_code: hash[&58usize].to_str(),
-            // field_template: ,
+            field_template: vec![Label {
+                reference_label: hash[&62usize].to_hash()[&5usize].to_str(),
+            }],
             crc1610: hash[&63usize].to_str(),
-            // templates: ,
+            templates: if templates.is_empty() {
+                None
+            } else {
+                Some(templates)
+            },
         }
     }
 }
@@ -66,11 +127,55 @@ mod test {
             merchant_category_code: 0000u32,
             merchant_name: "NOME DO RECEBEDOR".to_string(),
             merchant_city: "BRASILIA".to_string(),
+            merchant_information: vec![
+                MerchantInfo {
+                    id: 26,
+                    info: vec![
+                        Info {
+                            id: 0,
+                            info: "BR.GOV.BCB.PIX".to_string(),
+                        },
+                        Info {
+                            id: 1,
+                            info: "123e4567-e12b-12d1-a456-426655440000".to_string(),
+                        },
+                    ],
+                },
+                MerchantInfo {
+                    id: 27,
+                    info: vec![
+                        Info {
+                            id: 0,
+                            info: "BR.COM.OUTRO".to_string(),
+                        },
+                        Info {
+                            id: 1,
+                            info: "0123456789".to_string(),
+                        },
+                    ],
+                },
+            ],
             currency: "986".to_string(),
             postal_code: Some("70074900".to_string()),
             amount: Some(123.45),
             country_code: "BR".to_string(),
+            field_template: vec![Label {
+                reference_label: "RP12345678-2019".to_string(),
+            }],
             crc1610: "AD38".to_string(),
+            templates: Some(vec![Template {
+                id: 80usize,
+                info: vec![
+                    Info {
+                        id: 0usize,
+                        info: "BR.COM.OUTRO".to_string(),
+                    },
+                    Info {
+                        id: 1usize,
+                        info: "0123.ABCD.3456.WXYZ".to_string(),
+                    },
+                ],
+            }]),
         }
     }
 
