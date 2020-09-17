26410f00b866c882913cdb1c1d8b1df31d4e96d2
Julia Naomi <jnboeira@outlook.com>
2020-09-17 18:05:27 -0300

    to json in clj api

--- clj-brcode/src/clj_brcode/core.clj
+++ clj-brcode/src/clj_brcode/core.clj
@@ -6,6 +6,8 @@
     (let [lib-brcode (-> (gen-interface :name "LibC" :methods
                            [[edn_from_brcode [String] String] 
                             [edn_to_brcode [String] String]
+                            [json_from_brcode [String] String]
+                            [json_to_brcode [String] String]
                             [crc16_ccitt_from_message [String] String]])
                          LibraryLoader/create
                          (.load "brcode"))]
@@ -16,6 +18,12 @@
 
 (defn edn->brcode [edn]
   (-> mem-brcode (.edn_to_brcode (pr-str edn))))
+
+(defn brcode->json [s]
+  (-> mem-brcode (.json_from_brcode s)))
+
+(defn json->brcode [json]
+  (-> mem-brcode (.json_to_brcode json)))
 
 (defn crc16-ccitt [message]
   (-> mem-brcode (.crc16_ccitt_from_message message)))
--- clj-brcode/test/clj_brcode/core_test.clj
+++ clj-brcode/test/clj_brcode/core_test.clj
@@ -11,6 +11,13 @@
   (testing "conform edn to brcode"
     (is (= (edn->brcode edn) code))))
 
+(def json "{\"payload_version\":1,\"initiation_method\":null,\"merchant_account_information\":\"12345678901234\",\"merchant_information\":[{\"id\":26,\"info\":[{\"id\":0,\"info\":\"BR.GOV.BCB.PIX\"},{\"id\":1,\"info\":\"123e4567-e12b-12d1-a456-426655440000\"}]},{\"id\":27,\"info\":[{\"id\":0,\"info\":\"BR.COM.OUTRO\"},{\"id\":1,\"info\":\"0123456789\"}]}],\"merchant_category_code\":0,\"merchant_name\":\"NOME DO RECEBEDOR\",\"merchant_city\":\"BRASILIA\",\"postal_code\":\"70074900\",\"currency\":\"986\",\"amount\":123.45,\"country_code\":\"BR\",\"field_template\":[{\"reference_label\":\"RP12345678-2019\"}],\"crc1610\":\"AD38\",\"templates\":[{\"id\":80,\"info\":[{\"id\":0,\"info\":\"BR.COM.OUTRO\"},{\"id\":1,\"info\":\"0123.ABCD.3456.WXYZ\"}]}]}")
+(deftest json-brcode-test
+  (testing "conform rust result to clojure"
+    (is (= (brcode->json code) json)))
+  (testing "conform json to brcode"
+    (is (= (json->brcode json) code))))
+
 (def unchecked-code "00020104141234567890123426580014BR.GOV.BCB.PIX0136123e4567-e12b-12d1-a456-42665544000027300012BR.COM.OUTRO011001234567895204000053039865406123.455802BR5917NOME DO RECEBEDOR6008BRASILIA61087007490062190515RP12345678-201980390012BR.COM.OUTRO01190123.ABCD.3456.WXYZ6304")
 (deftest crc16-test
   (testing "biscuit is B8CE"
